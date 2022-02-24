486b1112def795f1499e3ea3905a4e665b34c5a3
Cr0wn-Gh0ul <troy.salem.work@gmail.com>
2022-02-24 11:41:24 -0500

    eth utils

---
+++ src/Util/Eth.js
@@ -0,0 +1,25 @@
+module.exports = {
+    hexToUint256Array: (hex) => {
+        try {
+            if (hex.length % 64 != 0) {
+                throw "Invalid length";
+            }
+            let uint256 = [];
+            for (let i = 0; i < hex.length; i += 64) {
+                uint256.push(BigInt("0x" + hex.slice(i, i + 64)).toString());
+            }
+            return uint256;
+        }
+        catch (ex) {
+            throw new Error("hexToUint256Array: ", ex)
+        }
+    },
+    removePubFromSig: (sig) => {
+        try {
+            return sig.slice(sig.length - 128);
+        }
+        catch(ex) {
+            throw new Error("removePubFromSig: ", sig)
+        }
+    }
+}
