ab21dd9c63610d6599643e51537a30052f622c4f
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2022-03-21 17:32:04 -0400

    Patch-build

--- src/DocTypes/index.js
+++ src/DocTypes/index.js
@@ -4,5 +4,4 @@
  * @typedef DataStoreAndIndexObject
  * @property { String } UTXOID - 32 Byte Hexadecimal Identifier for DataStore
  * @property { String } Index - 32 Byte Hex Identifier for the Index of this DataStore
- * @returns
  */
