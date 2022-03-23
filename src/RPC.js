946bf90611ab892d5fbf145cd1790c0dd18de88f
Adrian Bona <adrianbona@outlook.com>
2022-03-23 14:15:23 -0300

    Shift position

--- src/RPC.js
+++ src/RPC.js
@@ -325,7 +325,7 @@ class RPC {
             return false;
         }
         catch (ex) {
-            throw new Error(`RPC.getDataStoreByIndexBona: ${String(ex)}\r\n`);
+            throw new Error(`RPC.getDataStoreByIndexBona:\r\n${String(ex)}`);
         }
     }
 
