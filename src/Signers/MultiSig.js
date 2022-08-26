3eab539e314a362dd298acafead621de3ac64540
Ricardo <rfrabasile@gmail.com>
2022-08-26 18:55:01 -0400

    cleanup

--- src/Signers/MultiSig.js
+++ src/Signers/MultiSig.js
@@ -15,7 +15,7 @@ class MultiSig {
      * @param {Object} signer - Signer instance
      */
     constructor(Wallet, bnSigner) {
-        if(!(bnSigner instanceof BNSigner)) throw "bnSigner param must be an instance of BnSigner"
+        if(!(bnSigner instanceof BNSigner)) throw new Error("bnSigner param must be an instance of BnSigner");
         this.Wallet = Wallet;
         this.bnSigner = bnSigner;
         this.publicKeys = [];
