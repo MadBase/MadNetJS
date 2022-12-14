6d1f2e9a548106611ea6d4fc225bca30d4cbe68a
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2022-12-14 10:48:27 -0500

    chore: initial settings

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import json from '@rollup/plugin-json'
+
+export default [
+    {
+        input: "./index.ts",
+        output: [
+            {
+                file: "dist/esm/index.js",
+                format: "esm",
+                sourcemap: true,
+            },
+            {
+                file: "dist/cjs/index.js",
+                format: "cjs",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            json(),
+            typescript({ tsconfig: "./tsconfig.json" }),
+        ],
+        external: ["react", "react-dom", "styled-components"]
+    },
+];
