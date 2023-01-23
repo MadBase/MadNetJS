d63bbf6ffd56fb59fc4489ddaddf04317a2e3120
jona <306797+jona@users.noreply.github.com>
2023-01-23 08:41:58 -0800

    chore: update util exports

--- src/Util/Generic.ts
+++ src/Util/Generic.ts
@@ -1,10 +1,12 @@
-module.exports = {
-    /**
-     * Async sleep for specified ms
-     * @param ms - How long before the promise resolves
-     * @returns { Promise<null> }
-     */
-    sleep: async (ms: number): Promise<null> => {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-}
+/**
+ * Async sleep for specified ms
+ * @param ms - How long before the promise resolves
+ * @returns { Promise<null> }
+ */
+export const sleep = async (ms: number): Promise<null> => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+export default {
+    sleep,
+};
--- src/Util/String.ts
+++ src/Util/String.ts
@@ -1,15 +1,16 @@
-module.exports = {
-    /**
-     * Adds a trailing slash to a given string
-     * @param string
-     * @returns { string }
-     */
-    addTrailingSlash: (string: string): string => {
-        try {
-            return string.replace(/\/$|$/, '/');
-        }
-        catch (ex) {
-            throw new Error(`Can't add trailing slash to: ${string}`);
-        }
+/**
+ * Adds a trailing slash to a given string
+ * @param string
+ * @returns { string }
+ */
+export const addTrailingSlash = (string: string): string => {
+    try {
+        return string.replace(/\/$|$/, "/");
+    } catch (ex) {
+        throw new Error(`Can't add trailing slash to: ${string}`);
     }
-}
+};
+
+export default {
+    addTrailingSlash,
+};
